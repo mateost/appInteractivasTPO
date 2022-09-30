a358a353bf6bb30ee089e6a688f4362fb1730b93
spxger <spxger@gmail.com>
2022-09-30 02:49:14 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -59,7 +59,7 @@ export default function App() {
         {getRoutes(routes)}
         <Route path="/" element={<HeaderOne />} />
         <Route path="/presentation" element={<Presentation />} />
-        <Route path="*" element={<Navigate to="/presentation" />} />
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </ThemeProvider>
   );

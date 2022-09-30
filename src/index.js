b0de8493e849e9f660e1d451356259742666cd7e
spxger <spxger@gmail.com>
2022-09-30 00:15:25 -0300

    cambio de proyecto

--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
-import "./index.css";
+/**
+=========================================================
+* Material Kit 2 React - v2.0.0
+=========================================================
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <BrowserRouter>
+* Product Page: https://www.creative-tim.com/product/material-kit-react
+* Copyright 2021 Creative Tim (https://www.creative-tim.com)
+
+Coded by www.creative-tim.com
+
+ =========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import App from "App";
+
+ReactDOM.render(
+  <BrowserRouter>
     <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  </BrowserRouter>,
+  document.getElementById("root")
 );

dbe5eafcfc66fc27b820f28079e9004e64b8790a
spxger <spxger@gmail.com>
2022-11-16 16:17:18 -0300

    Update Protected.js

--- src/helpers/Protected.js
+++ src/helpers/Protected.js
@@ -1,6 +1,7 @@
 import { Navigate } from "react-router-dom";
 
 const Protected = ({ isLoggedIn, children }) => {
+    console.log(isLoggedIn)
     if (!isLoggedIn) {
         return <Navigate to="/login" replace />;
     }

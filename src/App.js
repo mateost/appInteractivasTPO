19e6a6a9c6718e53fb47878ae16df514685f138e
spxger <spxger@gmail.com>
2022-11-16 10:11:25 -0300

    update

--- src/App.js
+++ src/App.js
@@ -101,7 +101,7 @@ import { css } from "styled-components/macro"; //eslint-disable-line
 // import PrivacyPolicyPage from "pages/PrivacyPolicy.js";
 
 import ComponentRenderer from "ComponentRenderer.js";
-import MainLandingPage from "MainLandingPage.js";
+import MainLandingPage from "pages/MainLandingPage.js";
 import ThankYouPage from "ThankYouPage.js";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";

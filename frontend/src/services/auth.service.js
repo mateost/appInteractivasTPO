6062a4c1d07947ad6c6029606a4aee5bc980cf98
Mateo S <mateostornelli@hotmail.com>
2022-12-08 14:56:03 -0300

    aaaaa

--- frontend/src/services/auth.service.js
+++ frontend/src/services/auth.service.js
@@ -1,61 +1,86 @@
-
-export async function login(userName, password){
-    console.log("Mostrar Mas")
-    console.log(JSON.stringify({userName, password}));
-    return fetch(
-        'http://localhost:2032/api/login',
-        {
-            method: 'POST',
-            headers:{
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({userName, password})
-        })
-        .then((response) => {
-            console.log(JSON.stringify(response))
-            console.log("respuesta")
-            const respuesta = response.json()
-            if (response.ok) {
-                return respuesta;
-            }
-            throw new Error(response.status)
-        })
+export async function login(userName, password) {
+  console.log("Mostrar Mas");
+  console.log(JSON.stringify({ userName, password }));
+  return fetch("http://localhost:2032/api/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ userName, password }),
+  }).then((response) => {
+    console.log(JSON.stringify(response));
+    console.log("respuesta");
+    const respuesta = response.json();
+    if (response.ok) {
+      return respuesta;
+    }
+    throw new Error(response.status);
+  });
 }
-export async function checkExistUser(userName){
-    return fetch(
-        'http://localhost:2032/api/existUser',
-        {
-            method: 'POST',
-            headers:{
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({userName})
-        })
-        .then((response) => {
-            const respuesta = response.json()
-            if (response.ok) {
-                return respuesta;
-            }
-            throw new Error(response.status)
-        })
+export async function checkExistUser(userName) {
+  return fetch("http://localhost:2032/api/existUser", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ userName }),
+  }).then((response) => {
+    const respuesta = response.json();
+    if (response.ok) {
+      return respuesta;
+    }
+    throw new Error(response.status);
+  });
 }
-export async function create(email, password, nombre, tipo){
-    console.log("Mostrar Mas")
-    console.log(JSON.stringify({email, password, nombre, tipo}));
-    return fetch(
-        'http://localhost:2032/api/register',
-        {
-            method: 'POST',
-            headers:{
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({email, password, nombre, tipo})
-        })
-        .then((response) => {
-            const respuesta = response.json()
-            if (response.ok) {
-                return respuesta;
-            }
-            throw new Error(response.status)
-        })
+export async function create(
+  userName,
+  password,
+  nombre,
+  tipo,
+  telefono,
+  titulo,
+  experiencia,
+  fechaNacimiento,
+  estudios,
+  completado
+) {
+  console.log("Mostrar Mas");
+  console.log(
+    JSON.stringify({
+      userName,
+      password,
+      nombre,
+      tipo,
+      telefono,
+      titulo,
+      experiencia,
+      fechaNacimiento,
+      estudios,
+      completado,
+    })
+  );
+  return fetch("http://localhost:2032/api/register", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      userName,
+      password,
+      nombre,
+      tipo,
+      telefono,
+      titulo,
+      experiencia,
+      fechaNacimiento,
+      estudios,
+      completado,
+    }),
+  }).then((response) => {
+    const respuesta = response.json();
+    if (response.ok) {
+      return respuesta;
+    }
+    throw new Error(response.status);
+  });
 }
